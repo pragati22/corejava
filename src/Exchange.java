0586688be17043907628de323fdc510859ae533c
pragati22 <49032801+pragati22@users.noreply.github.com>
2020-09-15 17:17:03 +0530

    pragati agarwal
    cmd code for 1st assignment

---
+++ src/Exchange.java
@@ -0,0 +1,10 @@
+package output.conversion;
+
+public class Exchange{
+
+public static void main(String[] args){
+
+	Money mon = new Money();
+	System.out.println(mon.getCurrency(23));
+}
+}
---
+++ src/Money.java
@@ -0,0 +1,11 @@
+package com.conversion;
+
+public class Money{
+
+public int getCurrency(int i){
+
+ return i*73;
+
+}
+
+}
